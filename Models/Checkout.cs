4020363e2800bc9a70b5d1c537c5f80b6e023560
madaleineo <madaleineosmun1122@gmail.com>
2022-03-04 16:59:48 -0700

    yeah buddy

--- Models/Checkout.cs
+++ Models/Checkout.cs
@@ -14,33 +14,33 @@ namespace onlinebookstore.Models
         [BindNever]
         public ICollection<BasketLineItem> Lines { get; set; }
 
-        [Required(ErrorMessage = "Please enter a name: ")]
+        [Required(ErrorMessage = "Please enter a name ")]
 
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "Please enter the first address line name: ")]
+        [Required(ErrorMessage = "Please enter the first address line name ")]
 
         public string AddressLine1 { get; set; }
 
-        [Required(ErrorMessage = "Please enter a name: ")]
+        [Required(ErrorMessage = "Please enter a name ")]
 
         public string AddressLine2 { get; set; }
 
         public string AddressLine3 { get; set; }
 
-        [Required(ErrorMessage = "Please enter a city name: ")]
+        [Required(ErrorMessage = "Please enter a city name ")]
 
         public string City { get; set; }
 
-        [Required(ErrorMessage = "Please enter a state name: ")]
+        [Required(ErrorMessage = "Please enter a state name ")]
 
         public string State { get; set; }
 
-        [Required(ErrorMessage = "Please enter a valid zipcode: ")]
+        [Required(ErrorMessage = "Please enter a valid zipcode ")]
 
         public string Zip { get; set; }
 
-        [Required(ErrorMessage = "Please enter a country name: ")]
+        [Required(ErrorMessage = "Please enter a country name ")]
 
         public string Country { get; set; }
 
