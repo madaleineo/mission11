df58847ed1f6bfc13c9782f037239f923f799655
madaleineo <madaleineosmun1122@gmail.com>
2022-03-07 09:29:56 -0700

    fixed all the things

--- Components/CartSummaryViewComponent.cs
+++ Components/CartSummaryViewComponent.cs
@@ -1,19 +1,20 @@
-﻿//using System;
-//using onlinebookstore.Models;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using onlinebookstore.Models;
 
-//namespace onlinebookstore.Components
-//{
-//    public class CartSummaryViewComponent : TypesViewComponent
-//    {
-//        private Basket basket;
+namespace onlinebookstore.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
 
-//        public CartSummaryViewComponent(Basket b)
-//        {
-//            basket = b;
-//        }
-//        public ICheckoutRepository Invoke()
-//        {
-//            return View(basket);
-//        }
-//    }
-//}
+        public CartSummaryViewComponent(Basket b)
+        {
+            basket = b;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
